5d9639ae24a6565bc1f48811559e3c7f3a9a7f65
chowjx <chowjx1311@gmail.com>
2021-05-04 14:54:06 +0800

    Completed "3.1 Some simple math"

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,1 +1,12 @@
+# define variable a as my birthday as a 6 figure value
+a = 131100
+# define variable b as Rob's birthday as a 6 figure value
+b = 190784
+# define variable c as today's date as a 6 figure value 
+c = 100321
 
+d = a - c
+e = a - b
+
+# compare if value d is bigger than value e
+print(d>e)
