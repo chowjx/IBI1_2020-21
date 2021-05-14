f54bc15df83f54f8f8430414e58c1366cfe24793
chowjx <chowjx1311@gmail.com>
2021-05-14 21:46:40 +0800

    Added In Pseudocodes

--- Practical6/Coronavirus_Rate.py
+++ Practical6/Coronavirus_Rate.py
@@ -2,8 +2,12 @@
 Coronavirus_Rate = {'USA':29862124,'India':11285561,'Brazil':11205972,'Russia':4360823,'UK':4234924}
 print(Coronavirus_Rate)
 
+#Get the Keys from Coronavirus_Rate dictionary & store in a list
 labels = list(Coronavirus_Rate.keys())
 values = list(Coronavirus_Rate.values())
+#Get the Keys from Coronavirus_Rate dictionary & store in a list
+
+#Construct Pie Chart for Coronavirus_Rate using matplotlib
 import matplotlib.pyplot as plt
 plt.pie(values, labels=labels, autopct=lambda p:f'{p:.2f}%, {p*sum(values)/100 :.0f} cases')
 plt.show()
