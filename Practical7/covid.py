6c1569e3ed25cf3c845705232c76658257c5ec21
chowjx <chowjx1311@gmail.com>
2021-05-16 12:33:59 +0800

    Get total covid cases from Afghanistan only

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -21,12 +21,18 @@ print(covid_data.iloc[0,1]) # Get data from 1st row, 2nd column by row & column 
 print(covid_data.iloc[2,0:5]) # Get data from 3rd row, from columns 0 to 5
 print(covid_data.iloc[0:2,:]) # Get data from first 2 rows & all columns
 print(covid_data.iloc[0:10:2,0:5]) # Get data from every second row between & including 0 & 8, & from columns 0 to 5
-print(covid_data.iloc[0:12:2,:]) # Get data from every second row between & including 0 & 10, & from all columns
 
+# Get data from every second row between & including 0 & 10, & from all columns
+print(covid_data.iloc[0:12:2,:]) 
 
 print(covid_data.iloc[0:3,[0,1,3]])
 # Get data from first 3 rows but only the 1st, 2nd & 4th columns, below using Boolean
 my_columns = [True, True, False, True, False, False]
 print(covid_data.iloc[0:3, my_columns])
 
-print(covid_data.loc[2:4,"date"])
+print(covid_data.loc[2:4,"date"]) # Get data from row 2 to 4 & column named "date"
+print(covid_data.loc[0:81,"total_cases"]) # Get data from row 0 to 81 & column named "total_cases"
+
+# Get total cases from Afghanistan only (same output asc ovid_data.loc[0:81,"total_cases"])
+print(covid_data.loc[covid_data['location'] == 'Afghanistan','total_cases'])
+
