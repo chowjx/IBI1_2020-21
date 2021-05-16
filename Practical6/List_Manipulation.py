bd6d73fbac3cad147931716328683b2f739ef476
chowjx <chowjx1311@gmail.com>
2021-05-16 17:30:37 +0800

    Calculate average exon length & sort them

--- Practical6/List_Manipulation.py
+++ Practical6/List_Manipulation.py
@@ -1,1 +1,8 @@
+exon_counts = [51,1142,42,216,25,650,32533,57,1,523]
+gene_lengths = [9410,394141,4442,105338,19149,76779,126550,36296,842,15981]
+# Calculate average exon length by dividing gene lengths by exon counts
+average_exon_length = [m/n for m, n in zip(gene_lengths, exon_counts)]
+# Sort values in average exon length list ascendingly
+average_exon_length.sort()
+print(average_exon_length)
 
