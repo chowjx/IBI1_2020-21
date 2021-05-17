8d276c7d34a55cf539f33b84de07f73c7fee0ed5
chowjx <chowjx1311@gmail.com>
2021-05-17 20:30:46 +0800

    Create function in a class to return attributes

--- Practical10/Student_List.py
+++ Practical10/Student_List.py
@@ -1,1 +1,16 @@
+# Create a class called Student
+class Student:
+    def __init__(self, name, programme):
+        self.name = name
+        self.programme = programme
 
+    def introduce_student(self):
+        print('Student Name: ' + self.name, 'Undergrad Programme: ' + self.programme)
+
+# Set object with given student name and programme as attributes
+i = Student('Yang Min Mei', 'BMS') # Example
+ii = Student('Mason Leong Jie Ming', 'BMI') #Example
+
+# call function in class on objects for output
+i.introduce_student()
+ii.introduce_student()
