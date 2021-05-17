f5a6624bc505d38a575c288e9a764d6f1904f3bc
chowjx <chowjx1311@gmail.com>
2021-05-17 17:39:23 +0800

    function for reverse complement of a DNA seq

--- Practical10/Reverse_Complement_Calculator.py
+++ Practical10/Reverse_Complement_Calculator.py
@@ -1,1 +1,28 @@
+def complement_base(base):
+    """Returns  the Watson-Crick complement of a base"""
+    if base in 'Aa':
+        return 'T'
+    elif base in 'Tt':
+        return 'A'
+    elif base in 'Gg':
+        return 'C'
+    else:
+        return 'G'
 
+def reverse_complement(seq):
+    """Compute reverse complement of a sequence"""
+    
+    # initialise reverse complement
+    rev_seq = ''
+
+    # loop through & populate list with reverse complement
+    for base in reversed(seq):
+        rev_seq += complement_base(base)
+
+    print(rev_seq)
+
+# Example of calling the reverse complement function with sequences in uppercase
+reverse_complement('CGGATTGAC')
+
+# Example of calling the reverse complement function with sequences in lowercase
+reverse_complement('cggattgac')
