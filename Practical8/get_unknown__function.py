70cf531f37d798f30b747c9f638cccda9e0554ac
chowjx <chowjx1311@gmail.com>
2021-05-17 11:44:04 +0800

    import modules for os and regular expressions

--- Practical8/get_unknown__function.py
+++ Practical8/get_unknown__function.py
@@ -1,1 +1,8 @@
+# import modules for os and regular expressions
+import os
+import re 
 
+# Perform os functions to change directory, print current working directory & list files in current directory
+os.chdir("/Users/chowjx/Downloads")
+print(os.getcwd())
+print(os.listdir())
