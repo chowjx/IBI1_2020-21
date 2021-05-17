035f800dd14d76702e3739c363bc3358995fd038
chowjx <chowjx1311@gmail.com>
2021-05-17 22:36:55 +0800

    change name to first & last name

--- Practical10/Student_List.py
+++ Practical10/Student_List.py
@@ -1,15 +1,16 @@
 # Create a class called Student
 class Student:
-    def __init__(self, name, programme):
-        self.name = name
+    def __init__(self, first_name, last_name, programme):
+        self.first_name = first_name
+        self.last_name = last_name
         self.programme = programme
 
     def introduce_student(self):
-        print('Student Name: ' + self.name, 'Undergrad Programme: ' + self.programme)
+        print(self.first_name + " " + self.last_name + " " + self.programme)
 
 # Set object with given student name and programme as attributes
-i = Student('Yang Min Mei', 'BMS') # Example
-ii = Student('Mason Leong Jie Ming', 'BMI') #Example
+i = Student('Min Mei', 'Yang', 'BMS') # Example
+ii = Student('Jie Ming', 'Leong', 'BMI') #Example
 
 # call function in class on objects for output
 i.introduce_student()
