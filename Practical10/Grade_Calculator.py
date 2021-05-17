bb11f480b58d09b7705cb9c1e6f6eed0ae053ca8
chowjx <chowjx1311@gmail.com>
2021-05-18 00:01:18 +0800

    Added in pseudocodes

--- Practical10/Grade_Calculator.py
+++ Practical10/Grade_Calculator.py
@@ -1,12 +1,7 @@
-class Grade_Calculator:
-    def __init__(self, name, portfolio_grade, poster_grade, exam_grade):
-        self.name = name
-        self.portfolio_grade = portfolio_grade
-        self.poster_grade = poster_grade
-        self.exam_grade = exam_grade
-        
+class Grade_Calculator:      
+    # create function to get user to input student's name & scores
     def get_user_input():
-
+        # make variables accessible outside of the function as well
         global name
         global portfolio_grade
         global poster_grade
@@ -18,14 +13,17 @@ class Grade_Calculator:
         exam_grade = float(input("What's the % grade for your final exam?"))
         return name, portfolio_grade, poster_grade, exam_grade
     
+    # Create function to calculate final grade from the weighted percentages and scores
     def grade_calculation():
         Final_Grade = (portfolio_grade/100)*40 + (poster_grade/100)*30 + (exam_grade/100)*30
         print("Student's Name: " + name, "Final Grade: " + str(Final_Grade))
 
+# Create a function to call other functions in desired order
 def main():
     Grade_Calculator.get_user_input()
     Grade_Calculator.grade_calculation()
 
+# Call the main function that will call other functions to output entire program
 main()
 
 
