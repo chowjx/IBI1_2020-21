95b655cc80c2146bf5a5b67ba991c32bef7248c3
chowjx <chowjx1311@gmail.com>
2021-05-18 20:43:10 +0800

    use while loop to continue calculation to 5 rounds

--- Practical14/number_of_childNodes.py
+++ Practical14/number_of_childNodes.py
@@ -1,1 +1,10 @@
+from xml.dom.minidom import parse 
+import xml.dom.minidom
 
+DOMTree = xml.dom.minidom.parse('go_obo.xml')
+obo = DOMTree.documentElement
+terms = obo.getElementsByTagName('term')
+defs = terms.getElementsByTagName('def')
+for def in defs:
+    if def.hasAttribute('defstr'):
+        if
