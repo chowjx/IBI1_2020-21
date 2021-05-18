94a7673a503b14bd0627f831303ec4ac618e29ec
chowjx <chowjx1311@gmail.com>
2021-05-18 23:49:36 +0800

    Remove os functions

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -4,14 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Perform os functions to change directory, print current working directory & list files in current directory
-os.chdir("/Users/chowjx/Downloads")
-print(os.getcwd())
-print(os.listdir())
-
 # Read full_data.csv file into a dataframe object named covid_data via pandas
 covid_data = pd.read_csv("full_data.csv")
-print(covid_data)
 
 print(covid_data.head(5)) # Get first 5 rows of covid_data dataframe
 covid_data.info() # Get info of data type of data points in covid_data
