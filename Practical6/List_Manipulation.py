b347987e418423f03ed536d1c0d26e3f7c398995
chowjx <chowjx1311@gmail.com>
2021-05-18 16:55:23 +0800

    Round average exon length to whole number

--- Practical6/List_Manipulation.py
+++ Practical6/List_Manipulation.py
@@ -4,6 +4,7 @@ gene_lengths = [9410,394141,4442,105338,19149,76779,126550,36296,842,15981]
 average_exon_length = [m/n for m, n in zip(gene_lengths, exon_counts)]
 # Sort values in average exon length list ascendingly
 average_exon_length.sort()
+average_exon_length = [round(num) for num in average_exon_length]
 print('sorted average exon length:', average_exon_length)
 
 import matplotlib.pyplot as plt
