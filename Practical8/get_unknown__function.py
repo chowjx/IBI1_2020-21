6803d052359ce84922779736a6976340a3e92163
chowjx <chowjx1311@gmail.com>
2021-05-18 09:51:27 +0800

    remove list files, print all sequences

--- Practical8/get_unknown__function.py
+++ Practical8/get_unknown__function.py
@@ -2,10 +2,26 @@
 import os
 import re 
 
-# Perform os functions to change directory, print current working directory & list files in current directory
+# Perform os functions to change directory & print current working directory
 os.chdir("/Users/chowjx/Downloads")
 print(os.getcwd())
-print(os.listdir())
 
 allcdna = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa') # Open & read fasta file
-output = open('unknown_function.fa', 'w') # Create a fasta file to write output in it
+output = open('unknown_function.fa', 'a+') # Create a fasta file to write output in it
+
+name = ''
+seq = ''
+for line in allcdna:
+    if line[0] == '>':
+        if 'unknown function' in line:
+            output.write('\n')
+            output.write(str(len(line)))
+    elif line[0] != '>':
+        output.write(line)
+
+output.write(line.strip() +  '\n')
+
+
+
+
+
