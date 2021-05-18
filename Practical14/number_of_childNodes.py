c0261aa5c87bd5165ba5658ee5f97ed266685ce0
chowjx <chowjx1311@gmail.com>
2021-05-18 23:53:06 +0800

    Parse xml.file using DOM

--- Practical14/number_of_childNodes.py
+++ Practical14/number_of_childNodes.py
@@ -1,10 +1,69 @@
-from xml.dom.minidom import parse 
+from xml.dom.minidom import parse
 import xml.dom.minidom
+import matplotlib.pyplot as plt
+import numpy as np
 
-DOMTree = xml.dom.minidom.parse('go_obo.xml')
-obo = DOMTree.documentElement
-terms = obo.getElementsByTagName('term')
-defs = terms.getElementsByTagName('def')
-for def in defs:
-    if def.hasAttribute('defstr'):
-        if
+# Parsing XML using DOM
+DOMTree = xml.dom.minidom.parse("go_obo.xml")
+collection = DOMTree.documentElement
+terms = collection.getElementsByTagName("term")
+
+# Find the row containing <defstr> and confirm the type of molecule described in that row.
+def check_molecule(terms, molecule):
+    check_list = []
+    for term in terms:
+        defstr_text = term.getElementsByTagName("defstr")[0].childNodes[0]
+        lines = defstr_text.data
+        if molecule in lines:
+            check_list.append(term)
+    return check_list
+
+def get_childNodes(term_id, ids):
+    allChildren = []
+    for element in ids:
+        is_a = element.getElementsByTagName("is_a")
+        if not is_a:
+            allChildren.append(element)
+        else:
+            children_id = [child.childNodes[0].data for child in is_a]
+            children = []
+            for id in children_id:
+                children.append(term_id[id])
+            # recursive method to count all children
+            allChildren.append(element)
+            allChildren += get_childNodes(term_id, children)
+    return allChildren
+
+def count_childNodes(terms, molecule):
+    term_id_dict = {}
+    for term in terms:
+        id = term.getElementsByTagName("id")[0].childNodes[0].data
+        term_id_dict[id] = term
+    match_list = check_molecule(terms, molecule)
+    count = len(get_childNodes(term_id_dict, match_list)) - len(match_list)
+    return count
+
+# Run the function to get the result
+DNA = count_childNodes(terms, "DNA")
+RNA = count_childNodes(terms, "RNA")
+protein = count_childNodes(terms, "protein")
+glycoprotein = count_childNodes(terms, " glycoprotein")
+
+print("the number of childNodes for each DNA-related gene ontology term: " + str(DNA))
+print("the number of childNodes for each RNA-related gene ontology term: " + str(RNA))
+print("the number of childNodes for each protein-related gene ontology term: " + str(protein))
+print("the number of childNodes for each glycoprotein-related gene ontology term: " + str(glycoprotein))
+
+# To make the pie chart
+molecules = {"DNA": DNA, "RNA": RNA,
+             "Protein": protein, "Glycoprotein": glycoprotein}
+labels = molecules.keys()
+sizes = molecules.values()
+explode =(0, 0, 0, 0)
+fig1, ax1 = plt.subplots()    # ax1.xx, xx meams the operation to the figure
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')
+ax1.set(title ='The number of childNodes')
+
+plt.show()
