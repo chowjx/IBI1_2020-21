a0f48219b8079a9745f4c08e4db1b3cff02b8133
chowjx <chowjx1311@gmail.com>
2021-05-18 20:56:23 +0800

    write only sequences with unknown function

--- Practical8/get_unknown__function.py
+++ Practical8/get_unknown__function.py
@@ -7,21 +7,30 @@ os.chdir("/Users/chowjx/Downloads")
 print(os.getcwd())
 
 allcdna = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa') # Open & read fasta file
-output = open('unknown_function.fa', 'a+') # Create a fasta file to write output in it
+lines = allcdna.readlines()     # get the data from the allcdna fasta file
 
-name = ''
-seq = ''
-for line in allcdna:
-    if line[0] == '>':
-        if 'unknown function' in line:
-            output.write('\n')
-            output.write(str(len(line)))
-    elif line[0] != '>':
-        output.write(line)
+result = []
+for i in range(0, len(lines)):
+    if lines[i].startswith(">"):  # Locate the description
+        if 'unknown function' in lines[i]:  # Check whether sequence has an unknown function
+            result.append(re.findall(r'(>.+?)(?:_| )', lines[i])[0])
+            bases = ''
+            for n in range(0, len(lines[i:-1])):
+                if not lines[i+n+1].startswith(">"):  # Skip the description line
+                    bases += lines[i+n+1][:-1]
+                else:
+                    break
+            bases += "\n"
+            result.append(bases)
 
-output.write(line.strip() +  '\n')
+for i in range(0, len(result)):   # Read the length of the DNA sequence
+    if result[i].startswith(">"):
+        result[i] += "  "
+        result[i] += str(len(result[i+1]) - 1)
+        result[i] += "\n"
 
-
-
-
-
+with open('unknown_function.fa', 'w') as output: # Create a fasta file to write output in it
+    unknown_function = open('unknown_function.fa', "w")
+for line in result:     #To branch out; the names & sequences of different DNA are separated using a for loop
+    unknown_function.write(line)
+unknown_function.close()
