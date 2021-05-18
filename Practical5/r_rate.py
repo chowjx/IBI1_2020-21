c699ad9474b2a4bdfc8091d62abc5e6316563803
chowjx <chowjx1311@gmail.com>
2021-05-18 20:44:31 +0800

    while loop to repeat calculations till 5 rounds

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,12 +1,16 @@
-# total number of IBI1 students
+# Initial number of infected inidividuals
 n = 84
 
 # r rate variable
 r_rate = 1.1
 
 # calculate total no. of infections after 5 generations
-inf_num = n *r_rate *r_rate *r_rate *r_rate *r_rate
+i = 0
+while i < 5:
+    n = n + n * r_rate
+    i = i + 1
 
 # output
 print('R rate is the average number of individuals infected by each individual with the virus.')
-print('The total number of individuals infected after 5 generations is ' + str(int(inf_num)) + '.')
+print('The r rate is ' + str(r_rate) + '.')
+print('The total number of individuals infected after 5 generations is ' + str(int(n)) + '.')
