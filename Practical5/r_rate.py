d52f6ed84022f19e3abc2de20df929cdea6647ee
chowjx <chowjx1311@gmail.com>
2021-05-18 16:22:33 +0800

    calculate total no. of infections after 5 rounds

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,1 +1,12 @@
+# total number of IBI1 students
+n = 84
 
+# r rate variable
+r_rate = 1.1
+
+# calculate total no. of infections after 5 generations
+inf_num = n *r_rate *r_rate *r_rate *r_rate *r_rate
+
+# output
+print('R rate is the average number of individuals infected by each individual with the virus.')
+print('The total number of individuals infected after 5 generations is ' + str(int(inf_num)) + '.')
